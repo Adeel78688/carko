fb54f493ef547200aece1796fe50c982bff060c9
Adeel Tahir <adeelchoudhary78688@gmail.com>
2026-01-05 06:25:33 +0500

    Add files via upload

---
+++ author/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('profile/', views.profile, name='profile'),
+    path('profile/privacy_settings',
+         views.privacy_settings, name='privacy_settings'),
+    path('profile/edit_privacy_settings/',
+         views.edit_privacy_settings, name='edit_privacy_settings'),
+    path('signup/', views.UserSignUpView.as_view(), name='signup'), 
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('profile/password_change/', views.password_change, name='password_change'),
+    path('profile/password_change_without_old_password/', views.password_change_without_old_password,
+         name='password_change_without_old_password'),
+]
