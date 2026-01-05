d7353734b3fca47c492c42e0a310ecdd77565a85
Adeel Tahir <adeelchoudhary78688@gmail.com>
2026-01-05 06:27:08 +0500

    Add files via upload

---
+++ cars/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add_cars/', views.add_cars, name='add_cars'),
+    path('car_detail/<int:car_id>/', views.car_detail, name='car_detail'),
+]
